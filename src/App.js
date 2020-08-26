c9f7d5115859d12c670c28dbd7df31850c22e379
jfullerco <jonathan@jfuller.co>
2020-08-25 23:24:27 -0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'reactDOM';
+
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 import "./style.css";
 import "./ConnectDB.js";
 
 class App extends Component {
-<div> Sandbox </div>
+  render() {
+    return (
+<div>
+  <div className="title-main"> Sandboxed </div>
+      <div className="search-top">
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Client Search"
+            />
+              <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+              </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+    
+      </div>
+    <div className="menu-top"></div>
+    <div className="hero-nav"></div>
+    <div className="view-box"></div>
 
+
+
+</div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import App from "./App";
 
 
 ReactDOM.render(
-    <div>Sandbox</div>, document.getElementById('root')
+    <App />, document.getElementById('root')
 
 );
